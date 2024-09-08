08e07a9194f8ff3516abb6aeaf67452c6ea4da21
EviLuci <sujan.koju111@gmail.com>
2024-09-08 17:18:20 +0545

    flutter path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@
 # First line removes the path; second line sets it.  Without the first line,
 # your path gets massive and fish becomes very slow.
 set -e fish_user_paths
+set -Ux fish_user_paths $HOME/flutter/bin $fish_user_paths
 
 ### EXPORT ###
 set fish_greeting                                    # Supresses fish's intro message
