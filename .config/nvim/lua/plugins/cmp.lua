b7d09d446a856a39c5a7a93a20ffeb49c7020019
EviLuci <sujan.koju111@gmail.com>
2024-09-28 16:51:36 +0545

    minor changes, fixes and cleanups

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,10 +1,16 @@
 return {
-    {
-        "hrsh7th/nvim-cmp",
-        opts = function(_, opts)
-            table.insert(opts.sources, {
-                name = "Neorg"
-            })
-        end
-    }
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      table.insert(opts.sources, {
+        name = "Neorg"
+      })
+      table.insert(opts.sources, {
+        name = "supermaven",
+        group_index = 1,
+        priority = 100
+      })
+
+    end
+  }
 }
