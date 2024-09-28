9b1ad02511784eaa358e1d9f1126e7dd3b2190d1
EviLuci <sujan.koju111@gmail.com>
2024-09-29 03:57:55 +0545

    minor changes, fixes and cleanups

---
+++ .config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+  {
+    "roobert/surround-ui.nvim",
+    dependencies = {
+      "kylechui/nvim-surround",
+      "folke/which-key.nvim"
+    },
+    config = function()
+      require("surround-ui").setup({
+        root_key = "S"
+      })
+    end
+  }
+}
