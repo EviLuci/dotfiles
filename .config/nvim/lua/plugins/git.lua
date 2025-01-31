0ab2f775e2df7bea4e2b3e03b612652fdf0111d5
EviLuci <sujan.koju111@gmail.com>
2025-02-01 00:45:09 +0545

    some minor changes

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,25 +1,5 @@
 return {
   {
-    "NeogitOrg/neogit",
-    cmd = "Neogit",
-    dependencies = {
-      "sindrets/diffview.nvim" -- optional - Diff integration         -- optional
-    },
-    opts = {
-      integrations = {
-        diffview = true
-      },
-      disable_commit_confirmation = true
-    },
-    keys = {
-      {
-        "<leader>gn",
-        "<cmd>Neogit kind=tab<cr>",
-        desc = "Status"
-      }
-    }
-  },
-  {
     "lewis6991/gitsigns.nvim",
     opts = {
       signs = {
